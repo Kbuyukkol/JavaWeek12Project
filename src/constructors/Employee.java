4e2a413252b91dfb75ea6d1a0039fe6e543a9104
Kbuyukkol <109796185+Kbuyukkol@users.noreply.github.com>
2022-07-24 18:57:08 +0300

    Created employee class and fields

---
+++ src/constructors/Employee.java
@@ -0,0 +1,36 @@
+package constructors;
+
+public class Employee {
+
+
+    public String name, jobTitle;
+    public int ID;
+    public double salary;
+    public static String companyName;
+
+    public Employee(String name, String jobTitle, int ID, double salary) {
+        this.name = name;
+        this.jobTitle = jobTitle;
+        this.ID = ID;
+        this.salary = salary;
+
+    }
+
+    static{
+        companyName="Microsoft"; // this value is same for all the objects for this class
+
+    }
+
+    public Employee() { //default Constructor
+        salary=10000; // default for all employee created by this constructor
+    }
+
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", ID=" + ID +
+                ", salary=" + salary +
+                '}';
+    }
+}
